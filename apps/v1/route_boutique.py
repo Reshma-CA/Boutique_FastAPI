4c6029a7ad120d6e6b5327c870594b56d153e1bb
Reshma.CA <reshmacaci@gmail.com>
2024-11-29 12:37:45 +0530

    Sixth commit -1

--- apps/v1/route_boutique.py
+++ apps/v1/route_boutique.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from db.repository.boutique import list_boutique,retrieve_boutique
 from db.session import get_db
+from typing import Optional
 
 
 
@@ -13,7 +14,7 @@ router = APIRouter()
 @router.get("/")
 def home(request: Request, db: Session = Depends(get_db)):
     all_boutique = list_boutique(db=db)
-    context = {"request": request,"all_boutique": all_boutique }
+    context = {"request": request,"all_boutique": all_boutique}
     return templates.TemplateResponse("boutique/home.html",context = context ) # what is meta data
 
 
@@ -26,7 +27,3 @@ def boutique_detail(request: Request, id: int, db: Session = Depends(get_db)):
         "boutique/detail.html", {"request": request, "boutique_detail": boutique_detail}
     )
 
-# @router.get("app/boutique/{id}")
-# def boutique_detail(request:Request, id:int, db: Session = Depends(get_db)):
-#     boutique_detail = retrieve_boutique(id=id, db=db)
-#     return templates.TemplateResponse("boutique/detail.html",{"request":request,"boutique_detail":boutique_detail})
--- apps/v1/route_signup.py
+++ apps/v1/route_signup.py
@@ -1,5 +1,12 @@
-from fastapi import APIRouter,Request
+import json
+from fastapi import APIRouter,Request,Depends,responses,status,Form
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from db.session import get_db
+from schemas.user import UserCreate
+from db.repository.signup import create_new_user
+from pydantic.error_wrappers import ValidationError
 
 
 templates = Jinja2Templates(directory="templates")
@@ -8,3 +15,18 @@ router = APIRouter()
 @router.get("/signup")
 def signup(request:Request):
     return templates.TemplateResponse("auth/signup.html", {"request": request})
+
+@router.post("/signup")
+def signup(request:Request,username:str = Form(...),email:str = Form(...),
+           password:str = Form(...),confirm_password:str = Form(...),db:Session = Depends(get_db)):
+    errors = []
+    try:
+        user = UserCreate(username=username,email=email,password=password,confirm_password=confirm_password)
+        create_new_user(user=user,db=db)
+        return templates.TemplateResponse("auth/login.html", {"request": request})
+
+    except ValidationError as e:
+        error_list = json.loads(e.json())
+        for item in error_list:
+            errors.append(item.get("loc")[0]+":"+ item.get("msg"))
+        return templates.TemplateResponse("auth/signup.html", {"request": request, "errors": errors})
